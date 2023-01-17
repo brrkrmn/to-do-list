4d2358a3299a68f2b3229900c8954b25df16aa63
Berra Karaman <esma.berra.karaman@hotmail.com>
2023-01-17 11:02:13 +0300

    add event listener to display the new task on page

--- index.js
+++ index.js
@@ -37,10 +37,7 @@ function Task(title, due, priority, description) {
     this.title = title,
     this.due = due,
     this.priority = priority,
-    this.description = description,
-    this.sayName = function () {
-        console.log(this);
-    };
+    this.description = description
 }
 
 function createNewTask() {
@@ -49,9 +46,36 @@ function createNewTask() {
     const priority = document.getElementById("task-priority").value;
     const description = document.getElementById("task-description").value;
     const task = new Task(title, due, priority, description);
-    task.sayName();
+    createTaskItem(task);
 }
 
-addEventListenerToAddTaskButton();
-addEventListenerToDeleteFormButton();
-addEventListenerToSaveFormButton();
+function createTaskItem(task) {
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+    document.querySelector(".task-box").appendChild(taskItem);
+
+    const checkbox = document.createElement("button");
+    checkbox.classList.add("checkbox");
+
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const deleteTaskButton = document.createElement("button");
+    deleteTaskButton.classList.add("delete-task-button");
+
+    taskItem.append(checkbox, taskElement, deleteTaskButton);
+    addTaskToTaskBox(taskElement, task);
+}
+
+function addTaskToTaskBox(taskElement, task) {
+    console.log(task);
+    taskElement.textContent = task.title;
+}
+
+function addTask() {
+    addEventListenerToAddTaskButton();
+    addEventListenerToDeleteFormButton();
+    addEventListenerToSaveFormButton();
+}
+
+addTask();
